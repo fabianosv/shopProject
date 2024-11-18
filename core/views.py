0598fc1840322695cf8e4065754324ccb6f25b23
Adriel Vinicius <adriel1234.1234@gmail.com>
2024-11-18 08:41:04 -0400

    implentação do filtes.py cliente, venda e produto

--- core/views.py
+++ core/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django_filters.views import FilterView
 from .models import Cliente, Produto, Venda
 from .forms import ClienteForm, ProdutoForm, VendaForm
+from .filters import ClienteFilter, ProdutoFilter, VendaFilter
 
+
+# --- Cliente ---
 def cliente_list(request):
-    clientes = Cliente.objects.all()
-    return render(request, 'clientes.html', {'clientes': clientes})
+    filtro = ClienteFilter(request.GET, queryset=Cliente.objects.all())
+    return render(request, 'clientes.html', {'filter': filtro})
+
 
 def cliente_form(request, id=None):
-    cliente = Cliente.objects.get(id=id) if id else None
+    cliente = get_object_or_404(Cliente, id=id) if id else None
     if request.method == 'POST':
         form = ClienteForm(request.POST, instance=cliente)
         if form.is_valid():
@@ -17,8 +22,38 @@ def cliente_form(request, id=None):
         form = ClienteForm(instance=cliente)
     return render(request, 'cliente_form.html', {'form': form})
 
-# Similar para Produto e Venda
-# Funções `produto_list`, `produto_form`, `venda_list`, `venda_form`
-from django.shortcuts import render
 
-# Create your views here.
+# --- Produto ---
+def produto_list(request):
+    filtro = ProdutoFilter(request.GET, queryset=Produto.objects.all())
+    return render(request, 'produtos.html', {'filter': filtro})
+
+
+def produto_form(request, id=None):
+    produto = get_object_or_404(Produto, id=id) if id else None
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST, instance=produto)
+        if form.is_valid():
+            form.save()
+            return redirect('produto_list')
+    else:
+        form = ProdutoForm(instance=produto)
+    return render(request, 'produto_form.html', {'form': form})
+
+
+# --- Venda ---
+def venda_list(request):
+    filtro = VendaFilter(request.GET, queryset=Venda.objects.select_related('cliente', 'produto'))
+    return render(request, 'vendas.html', {'filter': filtro})
+
+
+def venda_form(request, id=None):
+    venda = get_object_or_404(Venda, id=id) if id else None
+    if request.method == 'POST':
+        form = VendaForm(request.POST, instance=venda)
+        if form.is_valid():
+            form.save()
+            return redirect('venda_list')
+    else:
+        form = VendaForm(instance=venda)
+    return render(request, 'venda_form.html', {'form': form})
